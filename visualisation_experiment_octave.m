3746c90f2b4e48f9c6e8b2754207df2d26ec3159
jamie-chapman <35632232+jamie-chapman@users.noreply.github.com>
2018-06-11 11:19:05 +0100

    Uploading Octave version.

--- visualisation_experiment_octave.m
+++ visualisation_experiment_octave.m
@@ -21,63 +21,63 @@ clc
 % D = [U1;U2;U3;U4];
 
 %Or just load a single user here
-D = load('');
+D = load('../user_data_dump/bcpurleigh/bcpurleigh_MATLAB.txt');
 
 %Processing----------------------------------------------------------------
 %Plotting------------------------------------------------------------------
 figure, hold on, axis tight
 
 subplot(5,2,1);
-histogram(D(:,1), 100, 'FaceColor', 'r');
+hist(D(:,1), 100, 'FaceColor', 'r');
 line([mean(D(:,1)), mean(D(:,1))], ylim, 'LineWidth', 2, 'Color', 'k');
 %line([(mean(D(:,1) + std(D(:,1)))), (mean(D(:,1) + std(D(:,1))))], ylim, 'LineWidth', 2, 'Color', 'g');
 %line([(mean(D(:,1) - std(D(:,1)))), (mean(D(:,1) - std(D(:,1))))], ylim, 'LineWidth', 2, 'Color', 'g');
 title('Popularity');
 
 subplot(5,2,2);
-histogram(D(:,2), 100, 'FaceColor', 'r');
+hist(D(:,2), 100, 'FaceColor', 'r');
 line([mean(D(:,2)), mean(D(:,2))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Dancability');
 
 subplot(5,2,3);
-histogram(D(:,3), 100, 'FaceColor', 'r');
+hist(D(:,3), 100, 'FaceColor', 'r');
 line([mean(D(:,3)), mean(D(:,3))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Energy');
 
 subplot(5,2,4);
-histogram(D(:,5), 100, 'FaceColor', 'r');
+hist(D(:,5), 100, 'FaceColor', 'r');
 line([mean(D(:,5)), mean(D(:,5))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Loudness');
 
 subplot(5,2,5);
-histogram(D(:,7), 100, 'FaceColor', 'r');
+hist(D(:,7), 100, 'FaceColor', 'r');
 line([mean(D(:,7)), mean(D(:,7))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Speechiness');
 
 subplot(5,2,6);
 %plot(D(:,8), zeros(1,size(D(:,8),1)), 'k.');
-histogram(D(:,8), 10, 'FaceColor', 'r');
+hist(D(:,8), 10, 'FaceColor', 'r');
 line([mean(D(:,8)), mean(D(:,8))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Acousticness');
 
 subplot(5,2,7);
 %plot(D(:,9), zeros(1,size(D(:,9),1)), 'k.');
-histogram(D(:,9), 10, 'FaceColor', 'r');
+hist(D(:,9), 10, 'FaceColor', 'r');
 line([mean(D(:,9)), mean(D(:,9))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Instrumentalness');
 
 subplot(5,2,8);
-histogram(D(:,10), 100, 'FaceColor', 'r');
+hist(D(:,10), 100, 'FaceColor', 'r');
 line([mean(D(:,10)), mean(D(:,10))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Liveness');
 
 subplot(5,2,9);
-histogram(D(:,11), 100, 'FaceColor', 'r');
+hist(D(:,11), 100, 'FaceColor', 'r');
 line([mean(D(:,11)), mean(D(:,11))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Valence');
 
 subplot(5,2,10);
-histogram(D(:,12), 100, 'FaceColor', 'r');
+hist(D(:,12), 100, 'FaceColor', 'r');
 hold on;
 line([mean(D(:,12)), mean(D(:,12))], ylim, 'LineWidth', 2, 'Color', 'k');
 title('Tempo');
